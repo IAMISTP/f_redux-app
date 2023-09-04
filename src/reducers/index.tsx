cb293447f11ff2c46545e51e4cebefbff0a07036
박혜민 <hyeminpak@gmail.com>
2023-09-04 15:00:32 +0900

    CombineReducers 를 이용해서 reducer 하나로 모으기

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
+import { combineReducers } from "redux";
+import counter from "./counter";
+import todos from "./todos";
+const rootReducer = combineReducers({
+    todos,
+    counter
+})
 
-const counter = (state = 0, action:{type:string}) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-}
-
-export default counter
+export default rootReducer
