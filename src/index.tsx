dcf620909c5e72699436f071a58cda08d7a2b9d6
박혜민 <hyeminpak@gmail.com>
2023-09-04 10:42:35 +0900

    store 생성

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const store = createStore(counter)
 root.render(
   <React.StrictMode>
     <App />
